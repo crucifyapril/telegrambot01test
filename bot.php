765fcfffd6f0d3a077069f3f6dba3b5504302494
claire <hauntingclaire@rambler.ru>
2024-08-18 15:52:35 +0300

    first commit

---
+++ bot.php
@@ -0,0 +1,19 @@
+<?php
+
+$token = '7273381403:AAE-EjxshXqFemoRaKrxrKYDpZtQgm6MluE'; // Замените на ваш токен
+$apiUrl = 'https://api.telegram.org/bot' . $token . '/';
+$webhookUrl = 'https://yourdomain.com/path/to/bot.php'; // Замените на URL вашего скрипта
+
+$url = $apiUrl . 'setWebhook';
+$data = [
+    'url' => $webhookUrl
+];
+$options = [
+    'http' => [
+        'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data),
+    ],
+];
+$context = stream_context_create($options);
+file_get_contents($url, false, $context);
